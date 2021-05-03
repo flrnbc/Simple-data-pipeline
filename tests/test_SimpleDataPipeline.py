be33f21018477f8c0ab3cfbc5d7638c61726a32c
Florian Beck <flrn.beck@gmail.com>
2021-05-03 13:34:35 +0200

    Add tests for SumpleDataPipeline.py

---
+++ tests/test_SimpleDataPipeline.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.impute import SimpleImputer
+import SimpleDataPipeline as sdp
+
+data_path = "datasets"
+data_url = (
+    "https://raw.githack.com/ageron/handson-ml2/master/datasets/housing/housing.tgz"
+)
+data_name = "housing"
+
+sdp.fetch_data(data_url, data_path, data_name)
+test_data = sdp.load_data_pd(data_path, data_name)
+
+
+def test_shuffle_split_data():
+    # test_data_short = test_data.iloc[:100]
+    splitting = pd.cut(
+        test_data["median_income"],
+        bins=[0, 1.5, 3.0, 4.5, 6, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+    strat_test_data = sdp.shuffle_split_data(test_data, splitting, ratio=0.2)
+
+    assert len(strat_test_data[0]) == 0.8 * len(test_data)
+    assert len(strat_test_data[1]) == 0.2 * len(test_data)
+
+
+def test_CombinedAttributesAdder():
+    adder = sdp.CombinedAttributesAdder([(0, 1), (1, 2)])
+
+    X = np.array([[2, 4, 8],
+                  [2, 4, 8],
+                  [4, 8, 16]])
+    X_adder = adder.fit_transform(X)
+
+    Y = np.array([[0.5, 0.5],
+                  [0.5, 0.5],
+                  [0.5, 0.5]])
+
+    assert (X_adder == np.c_[X, Y]).all()
+
+
+def test_num_pipeline():
+    pipeline_num = sdp.num_pipeline([(0, 1), (1, 2)])
+
+    X = np.array([[2, 4, 8, np.nan],
+                  [2, 4, 8, np.nan],
+                  [4, 8, 16, np.nan]])
+
+    # manually execute the steps in the pipeline
+    imputer = SimpleImputer(strategy="median")
+    X1 = imputer.fit_transform(X)
+    # combine attributes
+    Y = np.array([[0.5, 0.5],
+                  [0.5, 0.5],
+                  [0.5, 0.5]])
+    X2 = np.c_[X1, Y]
+    # scale
+    scaler = StandardScaler()
+    X3 = scaler.fit_transform(X2)
+
+    assert (X3 == pipeline_num.fit_transform(X)).all()
+
+
+def test_full_pipelin_tr():
+    X_np = np.array([[2, 4, 8, 6, "a"],
+                  [2, 4, 8, 8, "b"],
+                  [4, 8, 16, np.nan, "c"]])
+    X_df = pd.DataFrame(data=X_np,
+                        index=["1", "2", "3"],
+                        columns=["col 1", "col 2", "col3", "col4", "col5"])
+    print(X_df)
+    X_num = np.array([[2, 4, 8, 6],
+                  [2, 4, 8, 8],
+                  [4, 8, 16, np.nan]])
+
+    pipeline_num = sdp.num_pipeline([(0, 1), (1, 2)])
+    X1 = pipeline_num.fit_transform(X_num)
+    X_cat = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 1]])
+    Y = np.c_[X1, X_cat]
+    print(Y)
+
+    X_tr = sdp.full_pipeline_tr(X_df, [(0, 1), (1, 2)])
+    print(X_tr)
+    
+    #assert (X_tr == Y)
+
